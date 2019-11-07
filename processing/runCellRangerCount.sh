e6fb5f20302735a835c6c97a9fb3742dd4490d0a
Anne Senabouth <a.senabouth@garvan.org.au>
2019-11-08 09:54:30 +1100

    Updated run code

--- processing/runCellRangerCount.sh
+++ processing/runCellRangerCount.sh
@@ -7,7 +7,7 @@ source $CELLRANGER_PATH/sourceme.bash
 
 # Set up arguments for run
 SAMPLE=$1
-INPUT_DIR=/share/ScratchGeneral/annsen/data/experimental_data/PUBLISHING/RPE_scRNA
+INPUT_DIR=/share/ScratchGeneral/annsen/data/experimental_data/PUBLISHING/RPE_scRNA/$SAMPLE
 REF_DIR=/share/ClusterShare/thingamajigs/SCCG/data/reference_data/refdata-cellranger-GRCh38-3.0.0
 OUTPUT_DIR=/share/ScratchGeneral/annsen/data/experimental_data/CLEAN/RPE_scRNA_AgedStudy
 
@@ -15,6 +15,7 @@ cd $OUTPUT_DIR
 
 # Run Cell Ranger
 cellranger count --id=${SAMPLE} --sample=${SAMPLE} --fastqs=${INPUT_DIR} \
-    --localmem=128 --localcores=16 --transcriptome=${REF_DIR} --expect-cells=10000 --nosecondary || exit 1
+	--localmem=128 --localcores=16 --transcriptome=${REF_DIR} \
+	--expect-cells=10000 --nosecondary || exit 1
 
 
